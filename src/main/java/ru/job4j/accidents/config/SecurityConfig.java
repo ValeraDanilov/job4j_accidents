13adc7c4567eb406026a1fc2e573b419aa5dfb87
ValeraDanilov <delfiyets@gmail.com>
2023-08-09 19:27:47 +0300

    0. Spring Security   [#6879 #420844]

---
+++ src/main/java/ru/job4j/accidents/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package ru.job4j.accidents.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+}
